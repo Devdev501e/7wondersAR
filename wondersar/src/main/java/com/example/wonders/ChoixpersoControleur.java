d3e2be745276e037ff361d9da1b71ea4e68731d6
Devendran Vizearangame <dev.vizearangame@gmail.com>
2023-01-02 17:13:48 +0100

    update

--- wondersar/src/main/java/com/example/wonders/ChoixpersoControleur.java
+++ wondersar/src/main/java/com/example/wonders/ChoixpersoControleur.java
@@ -15,6 +15,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -36,18 +37,23 @@ public class ChoixpersoControleur implements Initializable {
     @FXML
     private TextField nameTextField;
     String[] wonderNames = {"Alexandrie", "Halicarnasse", "Ephese", "Olympie", "Babylone", "Rhodes", "Gizeh"};
+    ArrayList<String> wonderName=new ArrayList<>();
 
     private Stage stage;
     private Scene scene;
     private final Image image = new Image(getClass().getResourceAsStream("images/imagejeu/retour.png"));
     private final ImageView icon = new ImageView(image);
 
+
     private final Image image2 = new Image(getClass().getResourceAsStream("images/imagejeu/valider1.png"));
     private final ImageView icon2 = new ImageView(image2);
+    private final Image image3 = new Image(getClass().getResourceAsStream("images/imagejeu/Start games.png"));
+    private final ImageView icon3 = new ImageView(image3);
     private Parent root;
     private int nombre;
     private Wonder wonderChoice;
     private ArrayList<Player> players = new ArrayList<>();
+    Actions actions = new Actions();
 
 
 
@@ -66,11 +72,13 @@ public class ChoixpersoControleur implements Initializable {
     public void getNumber(Event event) {
         if (myChoiceBox.getValue()!=null) {
             nombre = Integer.parseInt(myChoiceBox.getValue());
-
+            for (Wonder wonders:Wonder.values()){
+                wonderName.add(wonders.getDisplayName());
+            }
             nameTextField.setVisible(true);
             myChoiceBox.setValue(null);
             myChoiceBox.getItems().removeAll(NP);
-            myChoiceBox.getItems().addAll(wonderNames);
+            myChoiceBox.getItems().addAll(wonderName);
 
             myChoiceBox.setOnAction(this::getWonderChoice);
         }
@@ -84,43 +92,44 @@ public class ChoixpersoControleur implements Initializable {
     public void onConfirmButton() {
         String name = nameTextField.getText();
         players.add(new Player(name, wonderChoice, new Hand(new ArrayList<>(), new ArrayList<>()),false, new ArrayList<>()));
+
+
         myChoiceBox.getItems().remove(wonderChoice.displayName);
         System.out.println(players);
 
         nameTextField.setText("");
 
         if (players.size() == nombre) {
+            startGame.setGraphic(icon3);
             valider.setVisible(false);
             startGame.setVisible(true);
         }
 
     }
 
-    public void switchScene()throws IOException{
-        if (myChoiceBox.getValue() != null) {
+    public void switchScene(Event event)throws IOException{
+
+
             //initialize game
-            Actions actions = new Actions();
+
 
             CardDecks mainDeck = new CardDecks("Main");
-            mainDeck.shuffleDeck();
-
-            ProgressTokens progressTokens = new ProgressTokens();
-
+            mainDeck.shuffleDeck();ProgressTokens progressTokens = new ProgressTokens();
             Conflict conflict = new Conflict(players.size());
 
             ArrayList<CardDecks> playerDecks = actions.createPlayerDecks(players);
-
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("..."));//"game.fxml"
-            Stage stage = (Stage) valider.getScene().getWindow();
-            Scene scene = new Scene(loader.load());
-
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Game.fxml"));
+            root=loader.load();
             GameController gameController = loader.getController();
             gameController.startTurn(players, mainDeck, playerDecks, conflict, progressTokens, 0);
 
+        stage = (javafx.stage.Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
+
         }
-    }
+
 
 
     public void retour (Event event) throws  IOException {
