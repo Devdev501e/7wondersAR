a8a99a6afaa113a672aa63cab610e997b070d6eb
lenaf000 <116339809+lenaf000@users.noreply.github.com>
2023-01-03 10:01:00 +0100

    gameController: essaie de faire afficher image

--- wondersar/src/main/java/com/example/wonders/GameController.java
+++ wondersar/src/main/java/com/example/wonders/GameController.java
@@ -11,15 +11,16 @@ import java.util.Objects;
 
 public class GameController {
     Card leftDeckCard;
-    @FXML
-    private final ImageView leftDeckCardImage = new ImageView();
     Card rightDeckCard;
-    @FXML
-    private final ImageView rightDeckCardImage = new ImageView();
     Card mainDeckCard;
     @FXML
-    private final ImageView mainDeckImage = new ImageView();
-    private final Image mainDeckBackPNG = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/wonders/images/cards/card-back/card-back-question.png")));
+    ImageView mainDeckImage = new ImageView();
+    @FXML
+    ImageView leftDeckCardImage = new ImageView();
+    @FXML
+    ImageView rightDeckCardImage = new ImageView();
+
+    private final Image mainDeckBackPNG = new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/cards/card-back/card-back-question.png")));
 
     private Player player;
     private final Actions actions = new Actions();
@@ -44,6 +45,7 @@ public class GameController {
         Image leftDeckCardPNG = new Image(Objects.requireNonNull(getClass().getResourceAsStream(leftDeckCard.front.imageResource)));
         leftDeckCardImage.setImage(leftDeckCardPNG);
 
+
         rightDeckCard = options.get(1).getCard(0);
         Image rightDeckCardPNG = new Image(Objects.requireNonNull(getClass().getResourceAsStream(rightDeckCard.front.imageResource)));
         rightDeckCardImage.setImage(rightDeckCardPNG);
@@ -51,24 +53,26 @@ public class GameController {
         mainDeckCard = mainDeck.getCard(0);
         Image mainDeckFrontPNG = new Image(Objects.requireNonNull(getClass().getResourceAsStream(mainDeckCard.front.imageResource)));
 
+        System.out.println(player.getChat());
         if (player.getChat()) {
             mainDeckImage.setImage(mainDeckFrontPNG);
         }
         else {
+            System.out.println("you are here");
             mainDeckImage.setImage(mainDeckBackPNG);
         }
     }
 
     public void onButtonLeftDeck() {
+        System.out.println("left");
         player.addCard(leftDeckCard, conflict.getAllConflicts());
         playerDecks.get(0).chooseCard();
-        System.out.println("left");
     }
 
     public void onButtonRightDeck() {
-        player.addCard(rightDeckCard, conflict.getAllConflicts());
-        playerDecks.get(1).chooseCard();
         System.out.println("right");
+        //player.addCard(rightDeckCard, conflict.getAllConflicts());
+        playerDecks.get(1).chooseCard();
     }
 
     public void onButtonMainDeck() {
