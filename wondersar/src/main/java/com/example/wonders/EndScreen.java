e315c0eadcdc572ff18b2bcd4f11b5ca5ab09faf
lenaf000 <116339809+lenaf000@users.noreply.github.com>
2023-01-13 13:15:50 +0100

    end screen sans test

--- wondersar/src/main/java/com/example/wonders/EndScreen.java
+++ wondersar/src/main/java/com/example/wonders/EndScreen.java
@@ -3,21 +3,60 @@ package com.example.wonders;
 import domain.ConstructionPiece;
 import domain.Player;
 import domain.ProgressToken;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.util.ArrayList;
 
 public class EndScreen {
+    @FXML
+    private ImageView imagePodium = new ImageView();
+    private Image podiumPNG = new Image(getClass().getResourceAsStream("images/imagejeu/podium.png"));
+    @FXML
+    private Label player1st;
+    @FXML
+    private Label player2nd;
+    @FXML
+    private Label player3rd;
+    @FXML
+    private Label loserLabel;
+    @FXML
+    private ImageView imageMedal = new ImageView();
+    private Image medalPNG = new Image(getClass().getResourceAsStream("images/imagejeu/1st_place.png"));
+    @FXML
+    private Label playerWinner;
+    ArrayList<Label> playerLabels = new ArrayList<>();
 
     public void getInfo(ArrayList<Player> players) {
-
+        playerLabels.add(player1st);
+        playerLabels.add(player2nd);
+        playerLabels.add(player3rd);
+        playerLabels.add(loserLabel);
+        Player[] playerOrder = calculateFinalPoints(players);
+        if (players.size() == 2) {
+            imageMedal.setImage(medalPNG);
+            playerWinner.setText(playerOrder[0].getName()+"("+playerOrder[0].getTotalPoints()+" points)");
+            loserLabel.setText(playerOrder[1].getName()+"("+playerOrder[1].getTotalPoints()+" points)");
+        }
+        else {
+            imagePodium.setImage(podiumPNG);
+            for (int i = 0; i < players.size(); i++) {
+                int num = Math.min(players.size()-1, 3);
+                playerLabels.get(num).setText(playerLabels.get(num).getText()+i+". "+playerOrder[i].getName()+"("+playerOrder[i].getTotalPoints()+" points)\n");
+            }
+        }
     }
 
-    public void calculateFinalPoints(ArrayList<Player> allPlayers) {
+    public Player[] calculateFinalPoints(ArrayList<Player> allPlayers) {
+        Player[] playerWinner = new Player[allPlayers.size()];
+        Player temp;
         for (Player i : allPlayers) {
             int cultureCount = 0;
 
-            //points vennant de jetons scientifique
             for (ProgressToken j : i.getAllTokens()) {
+                //points vennant de jetons scientifique
                 switch (j) {
                     case Decoration:
                         boolean totalCompletion = true;
@@ -27,7 +66,6 @@ public class EndScreen {
                                 break;
                             }
                         }
-
                         if (totalCompletion) {
                             i.setTotalPoints(i.getTotalPoints()+6);
                         } else {
@@ -59,10 +97,27 @@ public class EndScreen {
             }
 
             //points vennant de construction
+            for (ConstructionPiece j : i.getWonderContruction().getAllPieces()) {
+                i.setTotalPoints(i.getTotalPoints()+ j.getPoints());
+            }
 
             //points vennant de guerre
+            i.setTotalPoints(i.getTotalPoints()+i.getHand().getMilitaryPoints());
 
             //points vennant de carte bleu
+            i.setTotalPoints(i.getTotalPoints()+ i.getHand().getPointVictoire()[0]*2);
+            i.setTotalPoints(i.getTotalPoints()+ i.getHand().getPointVictoire()[1]*3);
         }
+
+        for (int i = 0; i < allPlayers.size(); i++) {
+            for (int j=1+i; j < allPlayers.size(); i++) {
+                if (allPlayers.get(i).getTotalPoints() > allPlayers.get(j).getTotalPoints()) {
+                    temp = allPlayers.get(i);
+                    playerWinner[i] = allPlayers.get(j);
+                    allPlayers.add(j, temp);
+                }
+            }
+        }
+        return playerWinner;
     }
 }
