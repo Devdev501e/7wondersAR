b2d18f267a1fa5de2c9390eeab97c2ef7b1b04d8
Devendran Vizearangame <dev.vizearangame@gmail.com>
2023-01-15 19:22:11 +0100

    63!

--- wondersar/src/main/java/com/example/wonders/HelloApplication.java
+++ wondersar/src/main/java/com/example/wonders/HelloApplication.java
@@ -3,9 +3,11 @@ package com.example.wonders;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
@@ -15,6 +17,7 @@ public class HelloApplication extends Application {
         stage.setTitle("7 Wonders: Architecture");
         stage.setScene(scene);
         stage.setResizable(false);
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/imagejeu/" + "main.png"))));
 
         stage.show();
     }
