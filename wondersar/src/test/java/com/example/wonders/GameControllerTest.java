582e1310d87c738d99b2ffc72405adf15bcb3a65
lenaf000 <116339809+lenaf000@users.noreply.github.com>
2023-01-26 21:26:31 +0100

    testScience

--- wondersar/src/test/java/com/example/wonders/GameControllerTest.java
+++ wondersar/src/test/java/com/example/wonders/GameControllerTest.java
@@ -7,11 +7,134 @@ import static org.junit.Assert.*;
 public class GameControllerTest {
     @Test
     public void getScienceEffectDuringGame() {
-        ProgressToken science = ProgressToken.Science;
-        Card card = new Card(CardType.CardScienceLaw, CardBack.Alexandrie);
         GameController gameController = new GameController();
-        boolean found = gameController.getScienceEffectDuringGame(science, card);
-        assertTrue(found);
+        ProgressToken[] progressTokens = {ProgressToken.Science, ProgressToken.Propaganda, ProgressToken.Jewelry, ProgressToken.Urbanism, ProgressToken.ArtsAndCrafts};
+        Card[] scienceCard = {new Card(CardType.CardScienceLaw, CardBack.Alexandrie), new Card(CardType.CardScienceMechanic, CardBack.Alexandrie), new Card(CardType.CardScienceArchitect, CardBack.Alexandrie)};
+        Card[] warCard = {new Card(CardType.CardWar_archer, CardBack.Alexandrie), new Card(CardType.CardWar_barbarian, CardBack.Alexandrie)};
+        Card[] materialGoldStone = {new Card(CardType.CardMaterialStone, CardBack.Alexandrie), new Card(CardType.CardMaterialGold, CardBack.Alexandrie)};
+        Card[] materialWoodBrick = {new Card(CardType.CardMaterialWood, CardBack.Alexandrie), new Card(CardType.CardMaterialBrick, CardBack.Alexandrie)};
+        Card[] materialPaperGlass = {new Card(CardType.CardMaterialPaper, CardBack.Alexandrie), new Card(CardType.CardMaterialGlass, CardBack.Alexandrie)};
+        Card[] others = {new Card(CardType.CardPolitic_cat, CardBack.Alexandrie), new Card(CardType.CardPolitic_emperor, CardBack.Alexandrie), new Card(CardType.CardWar_centurion, CardBack.Alexandrie)};
+        for (ProgressToken p :progressTokens) {
+            for (Card i : others) {
+                boolean found = gameController.getScienceEffectDuringGame(p, i);
+                assertFalse(found);
+            }
+            switch (p) {
+                case Urbanism:
+                    for (Card i : scienceCard) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : warCard) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialGoldStone) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialPaperGlass) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialWoodBrick) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertTrue(found);
+                    }
+                    break;
+                case ArtsAndCrafts:
+                    for (Card i : scienceCard) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : warCard) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialGoldStone) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialPaperGlass) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertTrue(found);
+                    }
+                    for (Card i : materialWoodBrick) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    break;
+                case Propaganda:
+                    for (Card i : scienceCard) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : warCard) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertTrue(found);
+                    }
+                    for (Card i : materialGoldStone) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialPaperGlass) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialWoodBrick) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    break;
+                case Jewelry:
+                    for (Card i : scienceCard) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : warCard) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialGoldStone) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertTrue(found);
+                    }
+                    for (Card i : materialPaperGlass) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialWoodBrick) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    break;
+                case Science:
+                    for (Card i : scienceCard) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertTrue(found);
+                    }
+                    for (Card i : warCard) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialGoldStone) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialPaperGlass) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    for (Card i : materialWoodBrick) {
+                        boolean found = gameController.getScienceEffectDuringGame(p, i);
+                        assertFalse(found);
+                    }
+                    break;
+            }
+        }
+
+
     }
 
     @Test
